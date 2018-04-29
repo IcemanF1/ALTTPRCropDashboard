f07b2487ff50e08330e40cf08b5b3256b5faf9b0
Hancin <hancin@hancinworld.com>
2018-04-29 15:46:29 -0400

    Force a camelCase JSON formatter on the HttpClient.

--- CropApi.vb
+++ CropApi.vb
@@ -1,22 +1,28 @@
 ﻿
 Imports System.Net.Http
+Imports System.Net.Http.Formatting
 Imports System.Net.Http.Headers
 Imports ALTTPRCropDashboard.Data
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Serialization
 
 Public Class CropApi
     Protected Property Client As New HttpClient
+    Protected Property Formatter As New JsonMediaTypeFormatter With {
+        .SerializerSettings = New JsonSerializerSettings With {.Formatting = Formatting.Indented, .ContractResolver = New CamelCasePropertyNamesContractResolver}
+    }
 
     Public Function GetCrops() As IEnumerable(Of RunnerInfo)
         Dim response = Client.GetAsync("v1/crops").Result
         If response.IsSuccessStatusCode Then
-            Return response.Content.ReadAsAsync(Of IEnumerable(Of RunnerInfo)).Result
+            Return response.Content.ReadAsAsync(Of IEnumerable(Of RunnerInfo))({Formatter}).Result
         End If
 
         Return Nothing
     End Function
 
     Public Sub CreateCrop(crop As RunnerCropAdd)
-        Dim response = Client.PostAsJsonAsync("v1/crops", crop).Result
+        Dim response = Client.PostAsync("v1/crops", crop, Formatter).Result
         response.EnsureSuccessStatusCode()
 
     End Sub
@@ -27,7 +33,6 @@ Public Class CropApi
         Client.DefaultRequestHeaders.Accept.Add(
             New MediaTypeWithQualityHeaderValue("application/json"))
         Client.Timeout = TimeSpan.FromSeconds(10)
-
     End Sub
 
     Private Sub New()
