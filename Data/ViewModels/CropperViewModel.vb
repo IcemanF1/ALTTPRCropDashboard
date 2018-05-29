dda2bdbb82fe1da61e8f8a144b2895ad9a91b049
David Pratte <d.pratte@napec.ca>
2018-05-29 14:07:30 -0400

    Data binding refactoring, part 1
    Data binding refactoring, part 2
    
    Data binding refactoring, part 3
    
    - Move "masterScale" and "masterSize" to the VM
    - Explicit "Private" decl. of vars instead of Dim.
    - Clean variable order.
    - Code Style adjustments to R# defaults
    
    Data binding refactoring, part 4
    
    - Put current size in VM
    - Put scale in VM
    - Unify all key press handlers in a single one
    - TopMost data binding
    - DispatchToObs helper method
    
    Data binding refactoring part 5
    
    -Attempt to remove useless function
    - More deduplication of code for isRightWindow
    
    BugFix - Lost unsaved changes when 2 runners updated
    
    Data Binding Refactoring, part 6
    
    -More dedup of isRightRunner code
    -Obs Connected behavior fix
    -Data bind all obs dependencies

---
+++ Data/ViewModels/CropperViewModel.vb
@@ -0,0 +1,19 @@
+﻿Namespace Data.ViewModels
+    Public Class CropperViewModel
+        Inherits ViewModelBase
+
+        Private _obsConnected As Boolean
+
+        Public ReadOnly Property LeftRunner As RunnerViewModel = New RunnerViewModel
+        Public ReadOnly Property RightRunner As RunnerViewModel = New RunnerViewModel
+
+        Public Property ObsConnected As Boolean
+            Get
+                Return _obsConnected
+            End Get
+            Set
+                SetField(_obsConnected, Value)
+            End Set
+        End Property
+    End Class
+End Namespace
---
+++ Data/ViewModels/ViewModelBase.vb
@@ -0,0 +1,27 @@
+﻿Imports System.ComponentModel
+Imports System.Runtime.CompilerServices
+
+Namespace Data.ViewModels
+    ''' <summary>
+    ''' Helper ViewModel class that implements INotifyPropertyChanged.
+    ''' Call SetField(field, value) on property setters to make it work.
+    ''' </summary>
+    Public MustInherit Class ViewModelBase
+        Implements INotifyPropertyChanged
+
+        Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+        Protected Overridable Sub OnPropertyChanged(propertyName As String)
+            RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
+        End Sub
+
+        Protected Function SetField(Of T)(ByRef field As T, value As T, <CallerMemberName> Optional propertyName As String = Nothing) As Boolean
+            If EqualityComparer(Of T).Default.Equals(field, value) Then
+                Return False
+            End If
+
+            field = value
+            OnPropertyChanged(propertyName)
+            Return True
+        End Function
+    End Class
+End Namespace
