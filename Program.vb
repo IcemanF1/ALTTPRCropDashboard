20a157dc734ac659ccc9e4137fd5ab21c719b460
Hancin <hancin@hancinworld.com>
2018-06-11 21:42:04 -0400

    Attempt setting store & reset

--- Program.vb
+++ Program.vb
@@ -1,16 +1,69 @@
 ﻿Imports System.Configuration
 Imports System.IO
 Imports System.Linq
+Imports System.Reflection
 Imports Squirrel
 
 Public Module Program
+    ' <summary>
+    ' Make a backup of our settings.
+    ' Used to persist settings across updates.
+    ' </summary>
+    Public Sub BackupSettings()
+        Dim settingsFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal).FilePath
+        Dim destination = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\..\\last.config"
+        File.Copy(settingsFile, destination, True)
 
-    Private Sub CheckForUpdate()
-        Task.Run(Async Function()
-                     Using updateMgr = New UpdateManager(If(ConfigurationManager.AppSettings("ReleasesURL"), "C:\TestReleases"))
-                         Return Await updateMgr.UpdateApp()
-                     End Using
-                 End Function)
+    End Sub
+
+    ' <summary>
+    ' Restore our settings backup if any.
+    ' Used to persist settings across updates.
+    ' </summary>
+    Private Sub RestoreSettings()
+        'Restore settings after application update            
+        Dim destFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal).FilePath
+        Dim sourceFile = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\..\\last.config"
+        ' Check if we have settings that we need to restore
+        If Not File.Exists(sourceFile) Then
+            Exit Sub
+
+        End If
+        ' Create directory as needed
+        Try
+            Directory.CreateDirectory(Path.GetDirectoryName(destFile))
+        Catch e As Exception
+        End Try
+
+
+        ' Copy our backup file in place 
+        Try
+            File.Copy(sourceFile, destFile, True)
+        Catch e As Exception
+        End Try
+
+        ' Delete backup file
+        Try
+            File.Delete(sourceFile)
+        Catch e As Exception
+        End Try
+
+        My.Settings.Reload()
+    End Sub
+
+
+
+    Private Sub CheckForUpdate(updatePath As String)
+        Task.Run(Async Sub()
+                     Try
+                         Using updateMgr = New UpdateManager(updatePath)
+                             BackupSettings()
+                             Await updateMgr.UpdateApp()
+                         End Using
+                     Catch ex As Exception
+                         MessageBox.Show($"Error checking for update: ${ex}")
+                     End Try
+                 End Sub)
     End Sub
     Private Sub CreateShortcuts(mgr As UpdateManager)
         Dim thePath = Path.Combine(Path.GetDirectoryName(Application.StartupPath), $"app-{mgr.CurrentlyInstalledVersion()}", "dashboard.exe")
@@ -35,20 +88,26 @@ Public Module Program
 
         Dim updatePath = If(ConfigurationManager.AppSettings("ReleasesURL"), testFolder)
 
-        If (updatePath <> testFolder OrElse System.IO.Directory.Exists(testFolder)) AndAlso
-            File.Exists(Path.Combine(Path.GetDirectoryName(Application.StartupPath), "Update.exe")) Then
-            Using updateMgr = New UpdateManager(If(ConfigurationManager.AppSettings("ReleasesURL"), "C:\TestReleases"))
+        If updatePath <> testFolder OrElse System.IO.Directory.Exists(testFolder) Then
+            Try
+                Using updateMgr = New UpdateManager(updatePath)
 
-                ' ReSharper disable AccessToDisposedClosure
-                SquirrelAwareApp.HandleEvents(
-                onInitialInstall:=Sub(v) CreateShortcuts(updateMgr),
-                onAppUpdate:=Sub(v) CreateShortcuts(updateMgr),
-                onAppUninstall:=Sub(v) DeleteShortcuts(updateMgr))
-                ' ReSharper enable AccessToDisposedClosure
+                    ' ReSharper disable AccessToDisposedClosure
+                    SquirrelAwareApp.HandleEvents(
+                    onInitialInstall:=Sub(v) CreateShortcuts(updateMgr),
+                    onAppUpdate:=Sub(v) CreateShortcuts(updateMgr),
+                    onAppUninstall:=Sub(v) DeleteShortcuts(updateMgr),
+                    onFirstRun:=Sub() RestoreSettings())
 
-            End Using
+                    ' ReSharper enable AccessToDisposedClosure
 
-            CheckForUpdate()
+                End Using
+
+                CheckForUpdate(updatePath)
+            Catch ex As Exception
+                MessageBox.Show($"Error checking for update: ${ex}")
+            End Try
+
 
         End If
 
