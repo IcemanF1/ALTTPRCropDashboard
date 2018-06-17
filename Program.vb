41956c30f62ce59a22065ce45ecb5dbf0829b68d
Hancin <hancin@hancinworld.com>
2018-06-17 15:03:10 -0400

    Insert v1.2.1.0 -> v1.2.2.0 handling code.
    This will be important to allow our first RTW update not to reset everyone's settings.

--- Program.vb
+++ Program.vb
@@ -15,20 +15,57 @@ Public Module Program
         File.Copy(settingsFile, destination, True)
 
     End Sub
+    ''' <summary>
+    ''' This code was built to handle the version setting upgrade from 1.2.6 to future versions. 
+    ''' </summary>
+    Private Sub HandleOldVersionSettingUpgrade()
+        Dim folderRoot = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                                      "ALTTPRandomizer")
 
+        Dim matchingFolders = New DirectoryInfo(folderRoot).GetDirectories().Where(Function(x) x.Name.StartsWith("croptool.exe_Url_"))
+
+        Dim checkSubPath = "1.2.1.0\user.config"
+
+        Dim found = False
+        Dim mostRecent As DateTime
+        Dim mostRecentPath As String = Nothing
+
+        For Each folder In matchingFolders
+            Dim targetPath = Path.Combine(folder.FullName, checkSubPath)
+            If File.Exists(targetPath) Then
+                Dim modified = File.GetLastWriteTime(targetPath)
+
+                If Not found OrElse modified > mostRecent Then
+                    mostRecent = modified
+                    mostRecentPath = targetPath
+                    found = True
+                End If
+
+            End If
+        Next
+
+        If found AndAlso mostRecentPath IsNot Nothing AndAlso File.Exists(mostRecentPath) Then
+            RestoreSettings(mostRecentPath, False)
+        End If
+
+    End Sub
     ' <summary>
     ' Restore our settings backup if any.
     ' Used to persist settings across updates.
     ' </summary>
-    Private Sub RestoreSettings()
+    Private Sub RestoreSettings(Optional sourceFile As String = Nothing, Optional runOldVersionUpgrade As Boolean = True)
         'Restore settings after application update            
         Dim destFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal).FilePath
-        MsgBox(destFile)
-        Dim sourceFile = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\..\\last.config"
+        If sourceFile Is Nothing Then
+            sourceFile = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\..\\last.config"
+            If runOldVersionUpgrade AndAlso Not File.Exists(sourceFile) Then
+                HandleOldVersionSettingUpgrade()
+                Exit Sub
+            End If
+        End If
         ' Check if we have settings that we need to restore
         If Not File.Exists(sourceFile) Then
             Exit Sub
-
         End If
         ' Create directory as needed
         Try
